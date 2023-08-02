c3225197c29d333e4271df8346be33ef4572bedd
franrbb <fran89rbb@hotmail.com>
2023-08-02 20:43:58 +0200

    creando microservicio inmuebles

---
+++ microservicio-inmuebles/src/main/java/com/springboot/inmuebles/app/MicroservicioInmueblesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.inmuebles.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioInmueblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioInmueblesApplication.class, args);
+	}
+
+}
---
+++ microservicio-inmuebles/src/test/java/com/springboot/inmuebles/app/MicroservicioInmueblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.inmuebles.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioInmueblesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
