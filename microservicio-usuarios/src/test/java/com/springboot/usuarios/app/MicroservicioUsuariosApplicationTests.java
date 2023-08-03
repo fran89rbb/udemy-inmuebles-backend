9410b652d60710fbe4da8952df2c91b549baea16
franrbb <fran89rbb@hotmail.com>
2023-08-03 17:27:30 +0200

    creacion microservicio usuarios

---
+++ microservicio-usuarios/src/test/java/com/springboot/usuarios/app/MicroservicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.usuarios.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
